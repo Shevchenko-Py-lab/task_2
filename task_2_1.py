2be6dfdf7e35c2a9593c2f4b68b92ecf477901fa
Shevchenko-Py-lab <83506735+Shevchenko-Py-lab@users.noreply.github.com>
2021-05-10 15:07:04 +0300

    Add files via upload

---
+++ task_2_1.py
@@ -0,0 +1,20 @@
+#1. Выяснить тип результата выражений:
+#15 * 3
+#15 / 3
+#15 // 2
+#15 ** 2
+
+#У меня какое-то странное ощущение, что я не так понял это задание
+first_type = 15 * 3
+second_type = 15 / 3
+third_type = 15 // 2
+fourth_type =15 ** 2
+
+print(first_type)
+print(type(first_type))
+print(second_type)
+print(type(second_type))
+print(third_type)
+print(type(third_type))
+print(fourth_type)
+print(type(fourth_type))
